84ca49ae59cdb5af77f229b297b3482f200aa89c
emmanuel-tientcheu <89963864+emmanuel-tientcheu@users.noreply.github.com>
2022-05-25 01:28:07 +0100

    Add files via upload

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -3,9 +3,17 @@ import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { Observable , of } from 'rxjs'; 
 import { db } from '../dbStructure'
 import { competance } from '../competanceStructure';
+import { experience } from '../experienceStructure';
+
 export class connectDb{
 
 }
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root'
@@ -33,6 +41,19 @@ export class DbServiceInformation{
   deleteCompetance(competance:competance){
     const url = `${this.UrlCompetance}/${competance.id}`;
     return this.http.delete(url);
+  }
+  
+  deleteExperience(cpt:experience){
+    const url = `${this.UrlExperiance}/${cpt.id}`;
+    return this.http.delete(url);
+  }
+
+  addSkillService(competance:competance){
+    return this.http.post<competance>(this.UrlCompetance , competance , httpOptions);
+  }
+
+  addExperienceService(cpt:experience){
+    return this.http.post<experience>(this.UrlExperiance , cpt , httpOptions)
   }
 }
 
